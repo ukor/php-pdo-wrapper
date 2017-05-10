7daebb6bb21400b14f70eb91df99b22d0fa37fb2
ukorjidechi <ukorjidechi@gmail.com>
2017-05-10 20:58:17 +0100

    Initial Commit

--- src/class.db.php
+++ src/class.db.php
@@ -14,9 +14,45 @@ class database{
 
     /**
      * db_connect constructor.: opens connection to database
-     * @param array $config
      */
-    function __construct(array $config){
+    function __construct(){
+
+        /**@host
+         * set the host, this is usually "localhost",
+         * ask you hosting company for this...
+         * @port
+         * Leave as it is, Change if you know what you doing
+         * @username
+         * change this to your database username,
+         * Recommend you create another user with less privilege
+         * it is usually root, if you not sure ask your hosting company for this
+         * @password
+         * change this to whatever password you set for database in user
+         * @database_name
+         * create a database with phpMyAdmin or from terminal,
+         * you can name it with any name that interest you
+         * but make sure to change the name here
+         * @charset
+         * set charset, leave this just as it is
+         * @table_prefix
+         * set database table prefix, change to what ever you like
+         */
+
+        $config = [
+            "host"=> "localhost",       /** usually localhost */
+
+            "port"=> 80,                /** leave as it is if not sure */
+
+            "username"=> "root",        /** Mysql username, usually "root". Recommend to create a new user with less privilege */
+
+            "password"=> "password",    /** password to MySQL username, leave blank if no password is set */
+
+            "database_name"=> "rose",
+
+            "charset" => "utfmb4",      /** leave as it is */
+
+            "table_prefix" => ""        /** optional */
+        ];
         try{
             $this->open_db_connection((array) $config);
         }catch(\PDOException $e){
