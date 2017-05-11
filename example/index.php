e90e6e332bbb09316e3fd09daf4a3521ccf458e9
ukorjidechi <ukorjidechi@gmail.com>
2017-05-11 13:43:03 +0100

    stable version 1.0.0

--- example/index.php
+++ example/index.php
@@ -4,12 +4,11 @@
  * @author Ukor Jidechi Ekundayo << https://ukorjidechi.com || ukorjidechi@gmail.com>>.
  * Date: 5/9/17
  * Time: 10:33 AM
+ *
+ * REMEMBER TO CHANGE YOUR USER NAME, PASSWORD AND DATABASE NAME IN __DIR__."/src/database.php
  */
 
-require_once (__DIR__."/../src/class.db.php");
-require_once (__DIR__."/../src/class.db_handler.php");
-//$foo = new \ukorJidechi\db($config);
+require_once (__DIR__."/../vendor/autoload.php");
 $bar = new \ukorJidechi\php_pdo_wrapper\database_handler();
 echo "<br /><pre>";
-print_r($foo);
 print_r($bar);
